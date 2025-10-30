bfedb2418208ead3463a2798eba4de161c8d1ab0
xapmw <zxcvbn1402qq@gmail.com>
2025-10-30 12:13:09 +0700

    cap nhat lan thu (3) jsk (jvm cua java)

--- src/main/java/runMain/HelloWorld_Java.java
+++ src/main/java/runMain/HelloWorld_Java.java
@@ -2,6 +2,7 @@ package runMain;
 
 public class HelloWorld_Java {
     public static void main(String[] args) {
+        System.out.println("Hello, World! from Java");
         HelloWorld_Scala.view();
         HelloWorld_KotlinKt.hello();
     }
